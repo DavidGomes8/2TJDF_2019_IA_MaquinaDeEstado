b806d0f762c7f402a0b4cee9ff92e3ba21e7b86b
Logon Aluno <logonaluno@local.com>
2019-08-13 10:50:22 -0300

    Anda Para

---
+++ Assets/Script/SMia.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SMia : MonoBehaviour
+{
+    public enum Estados
+    {
+        ESPERAR,
+        PATRULHAR
+    }
+
+    public Estados estadoAtual;
+
+    private Transform alvo;
+
+    private NavMeshAgent navMeshAgent;
+
+    [Header("Esperar")]
+    public float tempoEsperar = 2f;
+
+    public float tempoEsperando = 0f;
+
+    [Header("Patrulhar")]
+    public Transform waypint1;
+
+    public Transform waypint2;
+    private Transform waypintAtual;
+    public float distanciaMinimaWaypoint = 1f;
+    private float distanciaWaypointAtual;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        waypintAtual = waypint1;
+
+        Esperar();
+    }
+
+    private void Update()
+    {
+        ChecarEstados();
+    }
+
+    private void ChecarEstados()
+    {
+        switch (estadoAtual)
+        {
+            case Estados.ESPERAR:
+
+                if (EsperouTemposuficinete())
+                {
+                    Patrulhar();
+                }
+                else
+                {
+                    alvo = transform;
+                }
+                break;
+
+            case Estados.PATRULHAR:
+                if (PertoWypointAtual())
+                {
+                    Esperar();
+                    AlternarWaypoint();
+                }
+                else
+                {
+                    alvo = waypintAtual;
+                }
+
+                break;
+        }
+
+        navMeshAgent.destination = alvo.position;
+    }
+
+    #region ESPERAR
+
+    private void Esperar()
+    {
+        estadoAtual = Estados.ESPERAR;
+
+        tempoEsperando = Time.time;
+    }
+
+    private bool EsperouTemposuficinete()
+    {
+        return tempoEsperando + tempoEsperar <= Time.time;
+    }
+
+    #endregion ESPERAR
+
+    #region PATRULHAR
+
+    private void Patrulhar()
+    {
+        estadoAtual = Estados.PATRULHAR;
+    }
+
+    private bool PertoWypointAtual()
+    {
+        distanciaWaypointAtual = Vector3.Distance(transform.position, waypintAtual.position);
+        return distanciaWaypointAtual <= distanciaMinimaWaypoint;
+    }
+
+    private void AlternarWaypoint()
+    {
+        waypintAtual = (waypintAtual == waypint1) ? waypint2 : waypint1;
+    }
+
+    #endregion PATRULHAR
+}
---
+++ Assets/Script/SetDestination.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetDestination : MonoBehaviour
+{
+    public NavMeshAgent agente;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+
+                agente.destination = hit.point;
+            }
+        }
+    }
+}
